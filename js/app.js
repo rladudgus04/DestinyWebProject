bfc13e4d50853d402b9188020afa736301b5d1c2
rladudgus04 <totoyou1234@naver.com>
2021-05-12 17:32:21 +0900

    헌터 스킬창까지 완성함

--- js/app.js
+++ js/app.js
@@ -10,4 +10,22 @@ window.onload = function(){
     close.click(function(){
         bottom.animate({bottom : '-60%'});
     });
+
+
+    var skilItme1 = $(".item1");
+    var skilItem2 = $(".item2");
+    var skilItem3 = $(".item3");
+    var skilBack = $(".wrapper");
+
+    skilItme1.hover(function(){
+        skilBack.css('background-color', 'rgb(37, 107, 205)');
+    })
+    skilItem2.hover(function(){
+        skilBack.css('background-color', 'rgb(173, 81, 15)');
+    });
+    skilItem3.hover(function(){
+        skilBack.css('background-color', 'rgb(103, 9, 84)');
+    });
+    
+    
 }
